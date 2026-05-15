fa8f7795bfe6aad32251736cc3e107b8f3e02f81
karnyty <rrodny.12@gmail.com>
2026-05-15 16:35:28 +0000

    Add apartments controller, index view and CSS

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,4 +1,24 @@
 class ApartmentsController < ApplicationController
-    def index
-    end
+  def index
+		@pages = (params[:pages] || 1).to_i
+		@sort = params[:sort] || "price_asc"
+
+		if params[:search].present?
+
+			@apartments = OlxScraperService.new(pages: @pages).call
+			
+			@total = @apartments.count
+
+			@apartments = 
+			case @sort
+			when 'price_asc' then @apartments.sort_by {|apt| apt[:price].to_i}				
+			when 'price_desc' then @apartments.sort_by {|apt| apt[:price].to_i}.reverse
+			when 'title' then @apartments.sort_by {|apt| apt[:price]}
+			else @apartments
+			end	
+		else
+			@apartments = []
+			@total = 0 
+		end
+	end
 end
